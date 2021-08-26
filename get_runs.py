d051d247425468c5381ee329b0165aefc7f09e73
lbk-fishtown <79948589+lbk-fishtown@users.noreply.github.com>
2021-08-26 10:51:03 +1000

    Create get_runs.py
    Gets the most recent runs from dbt Cloud, their start/end time, status, and duration.
    
    Sample output:
    ```
    26550128	ERROR	unit test job	2021-08-26 00:04:21.982048+00:00	2021-08-26 00:05:01.491149+00:00	00:03:33
    26549763	ERROR	testing job	2021-08-26 00:04:19.841651+00:00	2021-08-26 00:04:32.558622+00:00	00:03:10
    26549468	SUCCESS	Production	2021-08-26 00:04:20.292452+00:00	2021-08-26 00:07:14.661411+00:00	00:05:57
    26549425	SUCCESS	Snapshot Example	2021-08-26 00:04:18.732717+00:00	2021-08-26 00:04:33.972439+00:00	00:03:17
    26549390	SUCCESS	dbt Vault	2021-08-26 00:04:19.520932+00:00	2021-08-26 00:07:50.417374+00:00	00:06:34
    26548993	ERROR	QA - Daily	2021-08-26 00:04:19.702292+00:00	2021-08-26 00:04:33.055266+00:00	00:03:59
    26548609	SUCCESS	Production	2021-08-26 00:04:18.098538+00:00	2021-08-26 00:05:46.618028+00:00	00:05:19
    26548529	SUCCESS	Daily Job	2021-08-26 00:04:18.382496+00:00	2021-08-26 00:06:51.723941+00:00	00:06:26
    26546175	ERROR	unit test job	2021-08-25 23:02:42.142502+00:00	2021-08-25 23:03:22.452569+00:00	00:02:20
    26545953	ERROR	testing job	2021-08-25 23:02:41.063529+00:00	2021-08-25 23:02:53.431748+00:00	00:01:54
    26545734	SUCCESS	Snapshot Example	2021-08-25 23:02:40.018424+00:00	2021-08-25 23:02:55.739198+00:00	00:02:00
    26545271	SUCCESS	Production	2021-08-25 23:02:40.229593+00:00	2021-08-25 23:03:49.358911+00:00	00:03:00
    26542958	ERROR	unit test job	2021-08-25 22:02:34.228331+00:00	2021-08-25 22:03:12.464785+00:00	00:02:20
    ```

---
+++ get_runs.py
@@ -0,0 +1,46 @@
+import requests
+import json
+from requests import api
+from requests.api import head
+import yaml
+import os
+import re
+import enum
+
+# define a class of different dbt Cloud API status responses in integer format
+class DbtJobRunStatus(enum.IntEnum):
+    QUEUED = 1
+    STARTING = 2
+    RUNNING = 3
+    SUCCESS = 10
+    ERROR = 20
+    CANCELLED = 30
+
+# set api token as environment variable or hardcode
+# this can be found in your profile in dbt Cloud
+api_token = os.getenv('DBT_CLOUD_API_KEY')
+#api_token = ''
+# set account_id & relevant job_id
+# this can be found in the url for the job in dbt Cloud
+account_id = 4238
+limit_num_of_runs = 100
+
+# set headers
+headers = {
+    'Authorization': f"Token {api_token}",
+    'Content-Type': 'application/json'
+}
+
+# endpoint for listing runs, limit to most recent XXX runs based on limit_num_of_runs
+runs_list_url = f"https://cloud.getdbt.com/api/v2/accounts/{account_id}/runs/?order_by=-id&limit={limit_num_of_runs}&include_related=[\"job\"]"
+
+# retrieve most recent runs, and parse response for run_id
+try:
+    runs_list = requests.get(runs_list_url, headers=headers)
+    runs_list = json.loads(runs_list.content)
+except Exception as e:
+    raise SystemExit(e)
+
+# print run details
+for run in runs_list['data']:
+    print(run['id'], DbtJobRunStatus(run['status']).name, run['job']['name'], run['started_at'], run['finished_at'], run['duration'], sep='\t')
